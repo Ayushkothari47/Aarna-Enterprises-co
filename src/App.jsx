b8cf27f4cc7b15dbce4ed28e77f0b2ba69a85248
Saurabh-Techie <sk2009025@gmail.com>
2025-08-31 21:09:27 +0530

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Login from "./components/admin/Login"
+import UserHome from "./components/user/UserHome"
+import UserLogin from "./components/user/UserLogin"
+function App() {
+
+  return (
+    <>
+      <h1>Hey ayush bby...</h1>
+      <Login/> 
+      <UserLogin />
+      <UserHome />
+      <h1>
+        this all content come form the app.jsx which is inside the src folder .........😎
+      </h1>
+    </>
+  )
+}
+
+export default App
