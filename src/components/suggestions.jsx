02ae8f2ba708f1b4f374efaa69947f17b84b1836
Ayushkothari47 <ayushkotharii47@gmail.com>
2025-09-05 08:43:45 +0530

    Deployment issue Re-fixed

--- src/components/suggestions.jsx
+++ src/components/suggestions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../components/user/UserHome.css';
+import '../components/user/userHome.css';
 import kedarnath from '../assets/kedarnath.png';  // Default image for testing (you can customize per route)
 
 const TravelSuggestions = () => {
