326a30195e9d690578305455fa9cc7be58d878ea
Ayushkothari47 <ayushkotharii47@gmail.com>
2025-11-08 19:47:29 +0530

    CMS Routes Added

--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -12,12 +12,17 @@ router.put('/updateBannerVisibility', CMS_Controller.updateBannerVisibility)
 
 //For Packages
 router.get('/fetchAllPackages', CMS_Controller.fetchAllPackages);
-router.post("/updatePackage", multer.fields([
-  { name: "thumbnail_url", maxCount: 1 },
-  { name: "img1", maxCount: 1 },
-  { name: "img2", maxCount: 1 },
-  { name: "img3", maxCount: 1 },
-]), CMS_Controller.updatePackage);
+router.post(
+  '/updatePackage',
+  upload.fields([
+    { name: 'thumbnail_url', maxCount: 1 },
+    { name: 'img1', maxCount: 1 },
+    { name: 'img2', maxCount: 1 },
+    { name: 'img3', maxCount: 1 },
+  ]),
+  CMS_Controller.updatePackage
+);
+
 
 
 module.exports = router;
