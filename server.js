0005484ccda1cb1059e794db12730c41b5abfe55
Ayushkothari47 <ayushkotharii47@gmail.com>
2025-11-09 19:28:23 +0530

    Parser added for form data

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // Connect Database
 connectDB();
