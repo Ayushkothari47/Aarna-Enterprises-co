5b31ee88dcc6056fd19100aa7ace7295e5195c49
Ayushkothari47 <ayushkotharii47@gmail.com>
2025-11-21 22:49:48 +0530

    Banner Fetch for admin

--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -15,6 +15,7 @@ router.delete('/deleteBanner',auth, CMS_Controller.deleteBanner)
 router.post('/uploadMobileBanner',auth, upload.array("banners"), CMS_Controller.uploadMobileBanners);
 router.post("/addMobileBanner", auth, upload.single("banner"), CMS_Controller.addMobileBanner);
 router.get('/fetchAllMobileBanner', CMS_Controller.getMobileBanners);
+router.get('/fetchAllMobileBanners', CMS_Controller.getAllMobileBanners);
 router.put('/updateBannerMobileVisibility',auth, CMS_Controller.updateMobileBannerVisibility)
 router.delete('/deleteMobileBanner',auth, CMS_Controller.deleteMobileBanner)
 
